fc8e084d77136f74b81bdbef15982b86a735c323
Edgar David Jimenez Jijon <brenedgar12@gmail.com>
2025-01-17 00:22:35 -0600

    correcion de cors

--- GestionArchivo/MyGestionArchivo/views.py
+++ GestionArchivo/MyGestionArchivo/views.py
@@ -7,11 +7,17 @@ from .serializer import *
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from rest_framework.permissions import AllowAny  # Para permitir POST sin autenticación
 class CustomUserViewSet(viewsets.ModelViewSet): 
-        permission_classes = [IsAuthenticated]
         queryset = Usuario.objects.all()
         serializer_class = CustomUserSerializers
+        
+        def get_permissions(self):
+                # Si el método es POST (registro de usuario), permitimos acceso sin autenticación
+                if self.action == 'create':
+                  return [AllowAny()]  # No requiere autenticación para crear
+                # Para las otras acciones (GET, PUT, PATCH, DELETE), se requiere autenticación
+                return [IsAuthenticated()]
 
 class  FileViewSet(viewsets.ModelViewSet): 
         permission_classes = [IsAuthenticated]
